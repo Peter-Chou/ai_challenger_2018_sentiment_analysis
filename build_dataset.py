2c36cbaa6bc9d72fd91f379091b3c4ccc302786d
Peter-Chou <zhou_00009@126.com>
2018-09-02 16:27:50 +0800

    use jieba to tokenize sentence

--- build_dataset.py
+++ build_dataset.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 creates or transforms the dataset e.g. sentences.txt, labels.txt
 """
@@ -5,9 +6,18 @@ creates or transforms the dataset e.g. sentences.txt, labels.txt
 import argparse
 import csv
 import re
-import unicodedata
+
+import jieba
+
+from model.helper import flatten
+
+# import unicodedata
+
+
+VOCAB_PATH = "./chinese_vectors/sgns.weibo.bigram-char"
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('--model_dir', default='experiments/base_model',
                     help="Directory containing params.json")
 parser.add_argument('--data_dir', default='data/small',
@@ -16,27 +26,46 @@ parser.add_argument('--restore_dir', default=None,
                     help="Optional, directory containing weights to reload before training")
 
 
-# test_line = "又是一家菜單讓人眼花繚亂的店！2.34蔡塘的蝴碟軒不在廣場美食區內，而是在萬佳酒店的大廳旁邊，裡面曲曲折折的分成很多用餐區，但其實桌數不少，用餐區隔得比較有私密感，適合小聚，裝潢跟它的菜色一樣走混搭風！\n\n蝴碟軒也是包山包海，口味就一般以上，達人未滿，如果交通方便在附近可以考慮，但不必要特地來吃，菜的份量很剛好，2人也可以點出花樣來。\n\n法國鵝肝炒飯：鵝肝味道很淡份量不多，所以應該把重點放在飯炒得有多香，一般般像媽媽菜，鍾意牛排的好吃很多。\n\n熟悉的墨魚仔：泰式涼拌海鮮口味，ok!\n\n生菜包養的肉碎：肉末炒四季豆，比較鹹一點，包在生菜中剛好，有辣度！\n\n月亮蝦薄餅：NG! 蝦在哪呢？月亮上嗎？ 請更名炸薄餅，但也炸得不好，油哈味很重。這45大洋我覺得太貴！"
+test_line = "🤔 又是一家菜单让人眼花缭乱的店！蔡塘的蝴碟轩不在广场美食区内，而是在万佳酒店的大厅旁边，里面曲曲折折的分成很多用餐区，但其实桌数不少，用餐区隔得比较有私密感，适合小聚，装潢跟它的菜色一样走混搭风！\n\n蝴碟轩也是包山包海，口味就一般以上，达人未满，如果交通方便在附近可以考虑，但不必要特地来吃，菜的份量很刚好，2人也可以点出花样来。\n\n🐓法国鹅肝炒饭：鹅肝味道很淡份量不多，所以应该把重点放在饭炒得有多香，一般般像妈妈菜，钟意牛排的好吃很多。\n\n🐙熟悉的墨鱼仔：泰式凉拌海鲜口味，ok!\n\n🐗生菜包养的肉碎：肉末炒四季豆，比较咸一点，包在生菜中刚好，有辣度！\n\n🌙月亮虾薄饼：NG! 虾在哪呢？月亮上吗？ 请更名炸薄饼，但也炸得不好，油哈味很重。这45大洋我觉得太贵！讓"
 
 # 判断中文范围
 # ! https://blog.csdn.net/JohinieLi/article/details/76152549
 
 
-# table = {ord(f):ord(t) for f,t in zip(
-#      u'，。！？【】（）％＃＠＆１２３４５６７８９０',
-#      u',.!?[]()%#@&1234567890')}
+def get_vocab(file_path):
+    word_set = set()
+    with open(file_path, newline='', encoding='utf-8', errors='ignore') as f:
+        reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
+        next(reader)    # 跳过第一行
+        for word, *_ in reader:
+            word_set.add(word)
+    return word_set
 
 
-def _remove_punctuation(line):
-    # rule = re.compile(ur"[^a-zA-Z0-9\u4e00-\u9fa5]")
-    # rule = re.compile("[^\u4e00-\u9fa5^.^a-z^A-Z^0-9]")
-    line = unicodedata.normalize("NFKC", line)
-    rule = re.compile("[^a-zA-Z0-9\u4e00-\u9fa5,.!?%#@&]")
+def _full_cut(word, vocab):
+    res = []
+    tmp = jieba.lcut(word, cut_all=True)
+    for i in (0, -1):
+        if tmp[i] in vocab:
+            res.append(tmp[i])
+    return res if len(res) > 0 else "<UNK>"  # 将vocab里未出现的word替换为<UNK>
+
+
+def tokenize_word(line, vocab):
+    rule = re.compile("[^a-zA-Z0-9\u4e00-\u9fa5]")
     line = rule.sub('', line)
-    return line
 
-
-# print(_remove_punctuation(test_line.decode('utf-8')))
+    sentence = []
+    for word in jieba.cut(line, cut_all=False):
+        if word in vocab:
+            try:
+                int(word)
+                sentence.append("<NUM>")  # 将数字 -> <NUM>
+            except ValueError:
+                sentence.append(word)
+        else:
+            sentence.append(_full_cut(word, vocab))
+    return list(flatten(sentence))
 
 
 def load_dataset(path_csv, test=False):
@@ -62,6 +91,10 @@ def load_dataset(path_csv, test=False):
 
 def main():
     args = parser.parse_args()
+    vocab = get_vocab(VOCAB_PATH)
+    print(tokenize_word(test_line, vocab))
+
+    # print(len(vocab))
 
 
 if __name__ == "__main__":
