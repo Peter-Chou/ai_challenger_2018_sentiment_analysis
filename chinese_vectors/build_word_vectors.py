4e172eab9a5c7dcf19c7f18283e826f19cc1551f
Peter-Chou <zhou_00009@126.com>
2018-09-08 19:31:26 +0800

    use bz2 to load file

--- chinese_vectors/build_word_vectors.py
+++ chinese_vectors/build_word_vectors.py
@@ -1,13 +1,14 @@
 """生成word转idx字典，以及vectors向量集"""
 
 import argparse
+import bz2
 import csv
 import json
 import os
 
 import numpy as np
 
-VOCAB_FILE = "./sgns.weibo.bigram-char"
+VOCAB_FILE = "./sgns.weibo.bigram-char.bz2"
 
 
 def build_word_idx_and_vectors(file_path):
@@ -20,7 +21,7 @@ def build_word_idx_and_vectors(file_path):
     """
 
     word_idx_dict = dict()
-    with open(file_path, newline='', encoding='utf-8', errors='ignore') as f:
+    with bz2.open(file_path, 'rt', newline='', encoding='utf-8', errors='ignore') as f:
         reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
         next(reader)    # 跳过第一行
         vectors = []
