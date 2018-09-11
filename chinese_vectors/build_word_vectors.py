b999972b16951ea2a0afef0d2f0cdca4f146509e
Peter-Chou <zhou_00009@126.com>
2018-09-11 20:21:35 +0800

    add <pad>=0 & remove normalize operation

--- chinese_vectors/build_word_vectors.py
+++ chinese_vectors/build_word_vectors.py
@@ -23,21 +23,23 @@ def build_word_idx_and_vectors(file_path):
     word_idx_dict = dict()
     with bz2.open(file_path, 'rt', newline='', encoding='utf-8', errors='ignore') as f:
         reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
-        next(reader)    # 跳过第一行
+        next(reader)
         vectors = []
-        for idx, (word, *vector) in enumerate(reader):
+        word_idx_dict["<PAD>"] = 0  # add <PAD>'s id is zero
+        vectors.append([0.0] * 300)
+        for idx, (word, *vector) in enumerate(reader, start=1):
             word_idx_dict[word] = idx
             vector = np.asarray([float(x) for x in vector if x != ''])
-            vector = vector / np.linalg.norm(vector)  # Frobenius norm
+            # // vector = vector / np.linalg.norm(vector)  # Frobenius norm
             vectors.append(vector)
         vectors = np.asarray(vectors)
 
     dict_save_path = os.path.join(os.path.dirname(
-        file_path), "word_idx_table.json")  # return word_idx_dict
+        file_path), "word_idx_table.json")
     vectors_save_path = os.path.join(os.path.dirname(
-        file_path), "vectors.npz")  # return word_idx_dict
+        file_path), "vectors.npy")
 
-    np.savez(vectors_save_path, vectors)
+    np.save(vectors_save_path, vectors)
     with open(dict_save_path, 'w', encoding='utf-8') as f:
         json.dump(word_idx_dict, f, indent=4, ensure_ascii=False)
 
