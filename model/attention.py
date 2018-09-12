b90012e40eed7f0104c3934702df2247facce214
Peter-Chou <zhou_00009@126.com>
2018-09-12 19:31:20 +0800

    add multi-head attention code

---
+++ model/attention.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+
+"""
+creates the deep learning model
+from https://github.com/Kyubyong/transformer/blob/master/modules.py
+"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def embed_lookup(inputs, vector_path, scale=False, scope="embedding"):
+
+    pretrained_embs = np.load(vector_path)
+    embed_size = pretrained_embs.shape[1]
+
+    with tf.variable_scope(scope):
+        pretrained_embs = tf.get_variable(
+            name="embs_pretrained",
+            initializer=tf.constant_initializer(
+                np.asarray(pretrained_embs), dtype=tf.float32),
+            dtype=tf.float64,
+            shape=pretrained_embs.shape, trainable=False)
+
+        num_vector = tf.get_variable(
+            name="NUM",
+            shape=[1, embed_size],
+            initializer=tf.random_uniform_initializer(-0.04, 0.04),
+            dtype=tf.float64,
+            trainable=True)
+        # pad_embedding = tf.get_variable(
+        #     name="PAD",
+        #     shape=[1, pretrained_embs.shape[1]],
+        #     initializer=tf.zeros_initializer(),
+        #     dtype=tf.float32,
+        #     trainable=False)
+        # 0 for <PAD> fixed vector, 1 ~ len(pretrained_vectors)-1 for trained vectors
+        # , len(pretrained_vectors) for trainable vector <NUM>
+        # embeddings = tf.concat(
+        #     [pad_embedding, pretrained_embs, num_vector], axis=0)
+        lookup_table = tf.concat(
+            [pretrained_embs, num_vector], axis=0)
+
+        output = tf.nn.embedding_lookup(lookup_table, inputs)
+
+        if scale:
+            outputs = outputs * (embed_size ** 0.5)
+
+    return output
+
+
+def positional_encoding(inputs,
+                        num_units,
+                        zero_pad=False,
+                        scale=True,
+                        scope="positional_encoding",
+                        reuse=None):
+    '''Sinusoidal Positional_Encoding.
+    Args:
+      inputs: A 3d Tensor with shape of (batch, N, T).
+      num_units: Output dimensionality
+      zero_pad: Boolean. If True, all the values of the first row (id = 0) should be constant zero
+      scale: Boolean. If True, the output will be multiplied by sqrt num_units(check details from paper)
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+    Returns:
+        A 'Tensor' with one more rank than inputs's, with the dimensionality should be 'num_units'
+    '''
+
+    N, T = inputs.get_shape().as_list()
+    with tf.variable_scope(scope, reuse=reuse):
+        position_ind = tf.tile(tf.expand_dims(tf.range(T), 0), [N, 1])
+
+        # First part of the PE function: sin and cos argument
+        position_enc = np.array([
+            [pos / np.power(10000, 2.*i/num_units) for i in range(num_units)]
+            for pos in range(T)])
+
+        # Second part, apply the cosine to even columns and sin to odds.
+        position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
+        position_enc[:, 1::2] = np.cos(position_enc[:, 1::2])  # dim 2i+1
+
+        # Convert to a tensor
+        lookup_table = tf.convert_to_tensor(position_enc)
+
+        if zero_pad:
+            lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
+                                      lookup_table[1:, :]), 0)
+        outputs = tf.nn.embedding_lookup(lookup_table, position_ind)
+
+        if scale:
+            outputs = outputs * num_units**0.5
+
+        return outputs
+
+
+def multihead_attention(queries,
+                        keys,
+                        num_units=None,
+                        num_heads=8,
+                        dropout_rate=0,
+                        is_training=True,
+                        causality=False,
+                        scope="multihead_attention",
+                        reuse=None):
+    '''Applies multihead attention.
+
+    Args:
+      queries: A 3d tensor with shape of [N, T_q, C_q].
+      keys: A 3d tensor with shape of [N, T_k, C_k].
+      num_units: A scalar. Attention size.
+      dropout_rate: A floating point number.
+      is_training: Boolean. Controller of mechanism for dropout.
+      causality: Boolean. If true, units that reference the future are masked.
+      num_heads: An int. Number of heads.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns
+      A 3d tensor with shape of (N, T_q, C)
+    '''
+    with tf.variable_scope(scope, reuse=reuse):
+        # Set the fall back option for num_units
+        if num_units is None:
+            num_units = queries.get_shape().as_list[-1]
+
+        # Linear projections
+        # Q = tf.layers.dense(queries, num_units, use_bias=False)  # (N, T_q, C)
+        # K = tf.layers.dense(keys, num_units, use_bias=False)  # (N, T_k, C)
+        # V = tf.layers.dense(keys, num_units, use_bias=False)  # (N, T_k, C)
+        # ? remove activation?
+        Q = tf.layers.dense(queries, num_units,
+                            activation=tf.nn.relu)  # (N, T_q, C)
+        K = tf.layers.dense(
+            keys, num_units, activation=tf.nn.relu)  # (N, T_k, C)
+        V = tf.layers.dense(
+            keys, num_units, activation=tf.nn.relu)  # (N, T_k, C)
+
+        # Split and concat
+        Q_ = tf.concat(tf.split(Q, num_heads, axis=2),
+                       axis=0)  # (h*N, T_q, C/h)
+        K_ = tf.concat(tf.split(K, num_heads, axis=2),
+                       axis=0)  # (h*N, T_k, C/h)
+        V_ = tf.concat(tf.split(V, num_heads, axis=2),
+                       axis=0)  # (h*N, T_k, C/h)
+
+        # Multiplication
+        outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
+
+        # Scale
+        outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
+
+        # Key Masking --- <PAD> is filled with all zeros
+        key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1)))  # (N, T_k)
+        key_masks = tf.tile(key_masks, [num_heads, 1])  # (h*N, T_k)
+        key_masks = tf.tile(tf.expand_dims(key_masks, 1), [
+                            1, tf.shape(queries)[1], 1])  # (h*N, T_q, T_k)
+
+        paddings = tf.ones_like(outputs)*(-2**32+1)
+        outputs = tf.where(tf.equal(key_masks, 0), paddings,
+                           outputs)  # (h*N, T_q, T_k)
+
+        # Causality = Future blinding
+        if causality:
+            diag_vals = tf.ones_like(outputs[0, :, :])  # (T_q, T_k)
+            tril = tf.contrib.linalg.LinearOperatorTriL(
+                diag_vals).to_dense()  # (T_q, T_k)
+            masks = tf.tile(tf.expand_dims(tril, 0), [
+                            tf.shape(outputs)[0], 1, 1])  # (h*N, T_q, T_k)
+
+            paddings = tf.ones_like(masks)*(-2**32+1)
+            outputs = tf.where(tf.equal(masks, 0), paddings,
+                               outputs)  # (h*N, T_q, T_k)
+
+        # Activation
+        outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
+
+        # Query Masking
+        query_masks = tf.sign(
+            tf.abs(tf.reduce_sum(queries, axis=-1)))  # (N, T_q)
+        query_masks = tf.tile(query_masks, [num_heads, 1])  # (h*N, T_q)
+        query_masks = tf.tile(tf.expand_dims(
+            query_masks, -1), [1, 1, tf.shape(keys)[1]])  # (h*N, T_q, T_k)
+        outputs *= query_masks  # broadcasting. (N, T_q, C)
+
+        # Dropouts
+        outputs = tf.layers.dropout(
+            outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+
+        # Weighted sum
+        outputs = tf.matmul(outputs, V_)  # ( h*N, T_q, C/h)
+
+        # Restore shape
+        outputs = tf.concat(tf.split(outputs, num_heads,
+                                     axis=0), axis=2)  # (N, T_q, C)
+
+        # Residual connection
+        outputs += queries
+
+        # Normalize
+        outputs = normalize(outputs)  # (N, T_q, C)
+
+    return outputs
+
+
+def feedforward(inputs,
+                num_units=[2048, 512],
+                scope="multihead_attention",
+                reuse=None):
+    '''Point-wise feed forward net.
+
+    Args:
+      inputs: A 3d tensor with shape of [N, T, C].
+      num_units: A list of two integers.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A 3d tensor with the same shape and dtype as inputs
+    '''
+    with tf.variable_scope(scope, reuse=reuse):
+        # Inner layer
+        params = {"inputs": inputs, "filters": num_units[0], "kernel_size": 1,
+                  "activation": tf.nn.relu, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+
+        # Readout layer
+        params = {"inputs": outputs, "filters": num_units[1], "kernel_size": 1,
+                  "activation": None, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+
+        # Residual connection
+        outputs += inputs
+
+        # Normalize
+        outputs = normalize(outputs)
+
+    return outputs
+
+
+def label_smoothing(inputs, epsilon=0.1):
+    '''Applies label smoothing. See https://arxiv.org/abs/1512.00567.
+
+    Args:
+      inputs: A 3d tensor with shape of [N, T, V], where V is the number of vocabulary.
+      epsilon: Smoothing rate.
+
+    For example,
+
+    ```
+    import tensorflow as tf
+    inputs = tf.convert_to_tensor([[[0, 0, 1],
+       [0, 1, 0],
+       [1, 0, 0]],
+      [[1, 0, 0],
+       [1, 0, 0],
+       [0, 1, 0]]], tf.float32)
+
+    outputs = label_smoothing(inputs)
+
+    with tf.Session() as sess:
+        print(sess.run([outputs]))
+
+    >>
+    [array([[[ 0.03333334,  0.03333334,  0.93333334],
+        [ 0.03333334,  0.93333334,  0.03333334],
+        [ 0.93333334,  0.03333334,  0.03333334]],
+       [[ 0.93333334,  0.03333334,  0.03333334],
+        [ 0.93333334,  0.03333334,  0.03333334],
+        [ 0.03333334,  0.93333334,  0.03333334]]], dtype=float32)]
+    ```
+    '''
+    K = inputs.get_shape().as_list()[-1]  # number of channels
+    return ((1-epsilon) * inputs) + (epsilon / K)
