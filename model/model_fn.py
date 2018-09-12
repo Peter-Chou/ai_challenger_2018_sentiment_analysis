ba5c124664f26aff6e1fc25ed517eba9b2f8f495
Peter-Chou <zhou_00009@126.com>
2018-09-12 19:30:49 +0800

    add position vectors to embeding vectors

--- model/model_fn.py
+++ model/model_fn.py
@@ -1,3 +1,24 @@
-"""
-creates the deep learning model
-"""
+"""Define the model."""
+
+import tensorflow as tf
+
+from model.attention import embed_lookup, positional_encoding
+
+
+def build_model(mode, vector_path, inputs, params, reuse=False):
+
+    sentence = inputs["sentence"]
+    label = inputs["label"]
+    # iterator_init_op = inputs["iterator_init_op"]
+
+    # build embedding vectors
+    sentence = embed_lookup(sentence, vector_path)
+    sentence += positional_encoding(tf.ones_like(inputs["sentence"]),
+                                    num_units=300,
+                                    zero_pad=False,
+                                    scale=False)
+    return sentence
+
+
+if __name__ == "__main__":
+    main()
