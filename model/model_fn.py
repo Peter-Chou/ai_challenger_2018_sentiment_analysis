97b23aacb0228fbef59751e00c43ead2b638c925
Peter-Chou <zhou_00009@126.com>
2018-09-12 20:21:36 +0800

    remove ambiguity variable names

--- model/model_fn.py
+++ model/model_fn.py
@@ -7,13 +7,13 @@ from model.attention import embed_lookup, positional_encoding
 
 def build_model(mode, vector_path, inputs, params, reuse=False):
 
-    sentence = inputs["sentence"]
+    x = inputs["sentence"]
     label = inputs["label"]
     # iterator_init_op = inputs["iterator_init_op"]
 
     # build embedding vectors
-    sentence = embed_lookup(sentence, vector_path)
-    sentence += positional_encoding(tf.ones_like(inputs["sentence"]),
+    sentence = embed_lookup(x, vector_path)
+    sentence += positional_encoding(x,
                                     num_units=300,
                                     zero_pad=False,
                                     scale=False)
