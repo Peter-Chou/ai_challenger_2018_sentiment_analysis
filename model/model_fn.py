dc89c65006d8e6d88dcb37f7b10031e3368d9aef
Peter-Chou <zhou_00009@126.com>
2018-09-15 10:08:28 +0800

    complete self attention part

--- model/model_fn.py
+++ model/model_fn.py
@@ -14,32 +14,58 @@ def build_model(mode, vector_path, inputs, params, reuse=False):
     # iterator_init_op = inputs["iterator_init_op"]
 
     # build embedding vectors
-    vector = embed_lookup(x, vector_path)
+    vector = embed_lookup(x, vector_path, scale=False)
+
+    # ! reduce the fiexed word dimensions to appropriate dimension
+    if params.hidden_size != vector.get_shape().as_list()[-1]:
+        # 原论文中使用全连接降维
+        # vector = tf.layers.dense(
+        #     vector,
+        #     params.hidden_size,
+        #     activation=None,
+        #     use_bias=False,
+        #     kernel_regularizer=tf.contrib.layers.l2_regularizer(1.0))
+
+        # ? (尝试) use conv1d to reduce the dimension (with shared weights)
+        vector = tf.layers.conv1d(
+            vector,
+            filters=params.hidden_size,
+            kernel_size=1,
+            kernel_regularizer=tf.contrib.layers.l2_regularizer(1.0))
+
+    # scale the word embedding
+    vector = vector * (params.hidden_size ** 0.5)
+
+    # 给词向量 增加位置信息
     vector += positional_encoding(x,
-                                  num_units=params.embed_size,
-                                  zero_pad=False,
+                                  num_units=params.hidden_size,
+                                  mask_pad=True,
+                                  #   zero_pad=False,
                                   scale=False)
 
     # # * add dropout mask vector may be not a good idea
     vector = tf.layers.dropout(vector, rate=params.dropout_rate,
                                training=tf.convert_to_tensor(is_training))
 
-    # transformer attention stacks
+    # # transformer attention stacks
+    # attns = []
     for i in range(params.num_attention_stacks):
         with tf.variable_scope(f"num_attention_stacks_{i + 1}"):
             # multi-head attention
             vector = multihead_attention(queries=vector,
                                          keys=vector,
-                                         num_units=params.embed_size,
+                                         num_units=params.hidden_size,
                                          num_heads=params.num_heads,
                                          dropout_rate=params.dropout_rate,
                                          is_training=is_training,
                                          causality=False)
 
-            # feed forward
+    #         # feed forward
             vector = feedforward(vector,
-                                 num_units=[4*params.embed_size, params.embed_size])
+                                 num_units=[4*params.hidden_size, params.hidden_size])
+    #         attns.append(vector)
 
+    # features = tf.concat(attns, 1)  # concat all attentions (like DenseNet)
 # ************************************************************
 # complete attention part, now CNN capture part
 # ************************************************************
